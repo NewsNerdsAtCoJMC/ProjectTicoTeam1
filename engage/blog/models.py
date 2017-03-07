d1f86dd758c34cd0a16a57aa8341515174b9a1ee
leraalfimova <v.alfimova@gmail.com>
2017-03-07 11:47:03 -0600

    Taking a go at blog models

--- engage/blog/models.py
+++ engage/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    name_slug = models.SlugField()
+    bio = models.TextField()
+    mugshot = models.ImageField(XXXXXXXX)
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(XXXXXXXXX)
+    cutline = models.TextField()
+    credit = models.ManyToManyField(Author)
+    
+
+
+class Post(models.Model):
+    headline = models.CharField(max_length=255)
+    text = models.TextField()
+    images = models.ManyToManyField(Image)
+    author = models.ManyToManyField(Author)
+    def __str__(self):
+        return self.headline
