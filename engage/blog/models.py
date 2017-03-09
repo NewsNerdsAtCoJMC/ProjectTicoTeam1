e1d94dbb14db6a82d93fcfe6b612848b159e208f
leraalfimova <v.alfimova@gmail.com>
2017-03-08 18:55:55 -0600

    first blog post (author + admin)

--- engage/blog/models.py
+++ engage/blog/models.py
@@ -4,15 +4,26 @@ class Author(models.Model):
     name = models.CharField(max_length=255)
     name_slug = models.SlugField()
     bio = models.TextField()
-    mugshot = models.ImageField(XXXXXXXX)
+    mugshot = models.ImageField(upload_to='Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/', height_field=200, width_field=200, max_length=100)
     def __str__(self):
         return self.name
+        return self.bio
+        return self.mugshot
 
 class Image(models.Model):
-    image = models.ImageField(XXXXXXXXX)
+    image = models.ImageField(upload_to='Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/', height_field=600, width_field=800, max_length=100)
     cutline = models.TextField()
     credit = models.ManyToManyField(Author)
+    def __str__(self):
+        return self.image
+        return self.cutline
+        return self.credit
+
     
+class Date(models.Model):
+    date = models.DateField()
+    def __str__(self):
+        return self.date
 
 
 class Post(models.Model):
@@ -20,5 +31,12 @@ class Post(models.Model):
     text = models.TextField()
     images = models.ManyToManyField(Image)
     author = models.ManyToManyField(Author)
+    date = models.DateField()
     def __str__(self):
         return self.headline
+        return self.text
+        return self.images
+        return self.author
+        return self.date
+
+    
--- engage/blog/views.py
+++ engage/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from blog.models import Author, Image, Date, Post
+
+def index(request):
+    author_line = Author.objects.all() 
+    context = {'author_line': author_line}
+    return render(request, 'blog/index.html', context)
+    
+    image_line = Image.objects.all() 
+    context = {'image_line': image_line}
+    return render(request, 'blog/index.html', context)
+
+    date_line = Date.objects.all() 
+    context = {'date_line': date_line}
+    return render(request, 'blog/index.html', context)
+
+    post_line = Post.objects.all() 
+    context = {'post_line': post_line}
+    return render(request, 'blog/index.html', context)
+
