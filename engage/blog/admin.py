e3ba7efe92ac333a035afcce610aeacbfe6c989d
leraalfimova <v.alfimova@gmail.com>
2017-03-14 10:46:58 -0500

    updated models

--- engage/blog/admin.py
+++ engage/blog/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Author
+from .models import Author, Post
 
 admin.site.register(Author)
+admin.site.register(Post)
+
 
--- engage/blog/models.py
+++ engage/blog/models.py
@@ -4,26 +4,16 @@ class Author(models.Model):
     name = models.CharField(max_length=255)
     name_slug = models.SlugField()
     bio = models.TextField()
-    mugshot = models.ImageField(upload_to='Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/', height_field=200, width_field=200, max_length=100)
+    mugshot = models.ImageField(upload_to="Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/", max_length=100, blank=True, null=True)
     def __str__(self):
         return self.name
-        return self.bio
-        return self.mugshot
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/', height_field=600, width_field=800, max_length=100)
+    image = models.ImageField(upload_to="Users/lera_alfimova/ProjectTicoTeam1/engage/engage/uploads/", height_field=600, width_field=800, max_length=100)
     cutline = models.TextField()
     credit = models.ManyToManyField(Author)
     def __str__(self):
-        return self.image
-        return self.cutline
-        return self.credit
-
-    
-class Date(models.Model):
-    date = models.DateField()
-    def __str__(self):
-        return self.date
+        return "%s" % self.image
 
 
 class Post(models.Model):
@@ -31,12 +21,5 @@ class Post(models.Model):
     text = models.TextField()
     images = models.ManyToManyField(Image)
     author = models.ManyToManyField(Author)
-    date = models.DateField()
     def __str__(self):
         return self.headline
-        return self.text
-        return self.images
-        return self.author
-        return self.date
-
-    
